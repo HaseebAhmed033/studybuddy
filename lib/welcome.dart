91584edb83a63a914bd726cf564477434251b919
Haseeb Ahmed <haseebahmed33@protonmail.com>
2025-11-05 15:41:06 +0500

    Splash screen Added with a logo

--- lib/welcome.dart
+++ lib/welcome.dart
@@ -1,55 +1,31 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const StudyBuddyApp());
-}
-
-class StudyBuddyApp extends StatelessWidget {
-  const StudyBuddyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'StudyBuddy',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: const WelcomeScreen(),
-    );
-  }
-}
-
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: const Color.fromARGB(255, 2, 55, 109), // dark navy blue
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            // 🔹 App Logo (temporary icon)
-            Icon(Icons.school, size: 100, color: Colors.blueAccent),
+            // 🖼️ Logo image
+            Image.asset(
+              'assets/images/logo.png', // make sure path matches pubspec.yaml
+              width: 150,
+              height: 150,
+            ),
 
             const SizedBox(height: 20),
 
-            // 🔹 Main Welcome Message
+            // 💬 Text
             const Text(
-              'Welcome to StudyBuddy 📚',
-              style: TextStyle(
-                fontSize: 26,
-                fontWeight: FontWeight.bold,
-                color: Colors.black87,
-              ),
-            ),
+              "Let's Study....Buddy🤍",
 
-            const SizedBox(height: 10),
-
-            // 🔹 Subtext
-            const Text(
-              'Your Smart Study Companion!',
-              style: TextStyle(fontSize: 16, color: Colors.grey),
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.white, fontSize: 18),
             ),
           ],
         ),
